67517c693842669cce95ef82f2d66cf70d223be5
SashaTyutyaev <sashatut2002@yandex.ru>
2024-03-19 19:55:19 +0300

    refactor: убрал ненужные аннотации

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -12,21 +12,8 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class User {
     private Integer id;
-
-    @Email(message = "Email должен быть в корректном формате")
-    @NotNull(message = "Email не должен быть null")
-    @NotBlank(message = "Email должен быть пустым")
     private String email;
-
-    @NotNull(message = "Логин не должен быть null")
-    @NotBlank(message = "Логин не должен быть пустым")
-    @Pattern(regexp = "^\\S+$", message = "Логин должен быть без пробелов")
     private String login;
-
     private String name;
-
-    @NotBlank(message = "Дата рождения не должна быть пустой")
-    @NotNull(message = "Дата рождения не должна быть null")
-    @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
 }
